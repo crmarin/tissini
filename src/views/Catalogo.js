d81ad0992cdc572760633e3bb65cc081edf331a7
cristian.marin <cristian.marin@safekeepingapp.com>
2022-06-11 17:30:40 -0500

    add cart

--- src/views/Catalogo.js
+++ src/views/Catalogo.js
@@ -1,5 +1,5 @@
 /*eslint-disable*/
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 import IndexNavbar from "components/Navbars/IndexNavbar.js";
 import Footer from "components/Footers/Footer.js";
@@ -12,6 +12,7 @@ export default function Index() {
   const dispatch = useDispatch();
   const { id } = useParams();
 
+  const [cart, setCart] = useState(0)
   const { catalogo, vendor } = useSelector((state) => state.userLogin);
 
   let categories = {};
@@ -23,20 +24,28 @@ export default function Index() {
     products = vendor.products
   }
 
+  const increment = () => {
+    // console.log('item :>> ', item);
+    setCart(cart+1);
+  };
+
+  const decrement = () => {
+    setCart(cart-1);
+  };
+
   useEffect(() => {
     if (id !== undefined) {
       dispatch(getcategories(id));
     }
   }, [dispatch, id]);
 
-  console.log(products);
   return (
     <>
       <IndexNavbar fixed />
 
       <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
         {
-          categories && categories.length > 0 &&
+          categories &&
           (
             <section className="pb-20 -mt-24">
               <div className="container mx-auto px-4">
@@ -91,42 +100,67 @@ export default function Index() {
       </div>
 
       <section className="pb-20 bg-blueGray-200 -mt-24">
-          <div className="container mx-auto px-4">
-            <div className="flex flex-wrap">
+        <div className="container mx-auto px-4">
+          <div className="flex flex-wrap">
 
-              {
-                products && products.length > 0 &&
-                products.slice(0, 30).map((item, id) => (
+            {
+              products && products.length > 0 &&
+              products.slice(0, 30).map((item, id) => (
 
-                  <div className="w-full lg:w-2/12 px-4 text-center" key={id}>
-                    <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                      <div className="px-4 py-5 flex-auto">
-                        <h6 className="text-xl font-semibold">{item.name}</h6>
-                        <Link
-                          to={`/catalogo/${item.id}`}
-                          className="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+                <div className="w-full lg:w-2/12 px-4 text-center" key={id}>
+                  <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                    <div className="px-4 py-5 flex-auto">
+                      <h6 className="text-xl font-semibold">{item.name}</h6>
+                      <img
+                        alt="..."
+                        src={`https://v3.tissini.app${item.images[0].url}`}
+                        className="w-full align-middle rounded-t-lg  inline-flex items-center justify-center h-60"
+                        onError={({ currentTarget }) => {
+                          currentTarget.onerror = null; // prevents looping
+                          currentTarget.src = "https://static.thenounproject.com/png/3843803-200.png";
+                        }}
+                      ></img>
+                      <p className="mt-2 mb-4 text-blueGray-500 capitalize">
+                        {item.reference} USD {item.price}
+                      </p>
+                      <div className="text-center flex justify-center mt-3 mb-6">
+                        <button
+                          className="bg-indigo-500 text-white active:bg-indigo-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                          type="button"
+                          onClick={decrement}
                         >
-                          <img
-                            alt="..."
-                            src={`https://v3.tissini.app${item.images[0].url}`}
-                            className="w-full align-middle rounded-t-lg  inline-flex items-center justify-center h-60"
-                            onError={({ currentTarget }) => {
-                              currentTarget.onerror = null; // prevents looping
-                              currentTarget.src = "https://static.thenounproject.com/png/3843803-200.png";
-                            }}
-                          ></img>
-                          <p className="mt-2 mb-4 text-blueGray-500 capitalize">
-                            {item.reference}
-                          </p>
-                        </Link>
+                          -
+                        </button>
+                        <input
+                          type="text"
+                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                          name="cart"
+                          value={cart}
+                          onChange={increment}
+                        />
+                        <button
+                          className="bg-purple-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                          type="button"
+                          onClick={increment}
+                        >
+                          +
+                        </button>
                       </div>
+                      <button
+                          className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                          type="button"
+                          // disabled={disabled}
+                        >
+                          Add to Cart
+                        </button>
                     </div>
                   </div>
+                </div>
 
-                ))}
-            </div>
+              ))}
           </div>
-        </section>
+        </div>
+      </section>
       <Footer />
     </>
   );
